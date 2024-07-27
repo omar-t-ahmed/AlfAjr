9d6d499060b81cfcaefd52c0fe4ab63aff51fa4d
hasan-huda <hasanhuda037@gmail.com>
2024-07-27 04:23:19 -0400

    Create Habit Page

--- src/app/create/habit/page.tsx
+++ src/app/create/habit/page.tsx
@@ -1,8 +1,18 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import React from 'react'
 
 const CreateHabit = () => {
   return (
-    <div>CreateHabit</div>
+    <main className="bg-zinc-900 h-screen text-white">
+    <MaxWidthWrapper className="py-4">
+      <div className="flex flex-col items-center">
+        <div className="font-bold text-3xl">
+          Create Habit
+        </div>
+      </div>
+    
+    </MaxWidthWrapper>
+    </main>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,27 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="bg-zinc-900 h-screen text-white">
-      <MaxWidthWrapper className="py-4">
-        <div className="flex flex-col items-center">
-          <div className="font-bold text-3xl mb-6">
-            Home
-          </div>
-          <>
-          <Link
-                  href="/create/habit"
-                  className={buttonVariants({
-                    size: "sm",
-                    className: "hidden sm:flex items-center gap-1 bg-green-600",
-                  })}
-                >
-                  {" "}
-                  Bismillah Create Habit Now
-                  <ArrowRight className="ml-1.5 h-5 w-5" />
-                </Link>
-          </>
-        </div>
-      </MaxWidthWrapper>
-    </main>
-  );
+    return (
+        <main className="bg-zinc-900 h-screen text-white">
+            <MaxWidthWrapper className="py-4">
+                <div className="flex flex-col items-center">
+                    <div className="font-bold text-3xl mb-6">Home</div>
+                    <>
+                        <Link
+                            href="/create/habit"
+                            className={buttonVariants({
+                                size: "sm",
+                                className:
+                                    "hidden sm:flex items-center gap-1 bg-green-600",
+                            })}
+                        >
+                            {" "}
+                            Bismillah Create Habit Now
+                            <ArrowRight className="ml-1.5 h-5 w-5" />
+                        </Link>
+                    </>
+                </div>
+            </MaxWidthWrapper>
+        </main>
+    );
 }
