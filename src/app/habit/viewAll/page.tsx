67a17e9948c26e0502e836a2c662826835326088
hasan-huda <hasanhuda037@gmail.com>
2024-07-27 20:06:59 -0400

    All Habits

--- src/app/habit/viewAll/page.tsx
+++ src/app/habit/viewAll/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import React from "react";
 
 const AllHabis = () => {
-  return (
-    <div>AllHabis</div>
-  )
-}
+    return (
+        <main className="bg-zinc-900 h-screen text-white">
+            <MaxWidthWrapper className="py-4">
+                <div className="flex flex-col items-center">
+                    <div className="font-bold text-4xl mb-6">All Habits</div>
+                </div>
+            </MaxWidthWrapper>
+        </main>
+    );
+};
 
-export default AllHabis
+export default AllHabis;
