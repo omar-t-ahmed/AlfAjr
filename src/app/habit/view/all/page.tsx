4ed7c544e01cb7f9aced623ce954614dfd8eb15d
hasan-huda <hasanhuda037@gmail.com>
2024-07-28 18:43:34 -0400

    Fix loading screen in view all

--- src/app/habit/view/all/page.tsx
+++ src/app/habit/view/all/page.tsx
@@ -25,7 +25,9 @@ interface UserWithHabits {
 }
 
 const AllHabits = () => {
-    const [userWithHabits, setUserWithHabits] = useState<UserWithHabits | null>(null);
+    const [userWithHabits, setUserWithHabits] = useState<UserWithHabits | null>(
+        null
+    );
     const { user } = useAuth();
     const router = useRouter();
 
@@ -40,12 +42,15 @@ const AllHabits = () => {
                     });
 
                     const userId = userResponse.data.id;
-                    const userWithHabitsResponse = await axios.get(`/api/users`, {
-                        params: {
-                            id: userId,
-                            includeHabits: true,
-                        },
-                    });
+                    const userWithHabitsResponse = await axios.get(
+                        `/api/users`,
+                        {
+                            params: {
+                                id: userId,
+                                includeHabits: true,
+                            },
+                        }
+                    );
 
                     setUserWithHabits(userWithHabitsResponse.data);
                 } catch (error) {
@@ -92,7 +97,8 @@ const AllHabits = () => {
                         href="/habit/create"
                         className={buttonVariants({
                             size: "lg",
-                            className: "bg-green-600 mt-5 sm:flex items-center gap-1 mb-6",
+                            className:
+                                "bg-green-600 mt-5 sm:flex items-center gap-1 mb-6",
                         })}
                     >
                         Create New Habit
@@ -100,54 +106,89 @@ const AllHabits = () => {
                     </Link>
                     {userWithHabits ? (
                         <div className="w-full px-6 sm:px-2">
-                                <div className="text-3xl mb-4 text-bold mt-8">
-                                    <span className="hidden md:flex">Habits for {userWithHabits.username}</span>
-                                    {userWithHabits.username  === "tsheikh2001"? <div className="text-bold text-blue-500">Tablighi Edition</div> :""}
-                                </div>
-                                <div className="grid grid-cols-1 gap-y-6 ">
-                                    {userWithHabits.habits.map((habit) => (
-                                        <Card
-                                            key={habit.id}
-                                            className={`text-white ${getColor(habit.worship)} transform transition-transform duration-300 hover:scale-105 cursor-pointer ring-4 ring-black`}
-                                            onClick={() => handleCardClick(habit.id)}
-                                        >
-                                            <CardHeader>
-                                                <CardTitle className="text-xl text-center">{habit.worship}</CardTitle>
-                                            </CardHeader>
-                                            <CardContent>
+                            <div className="text-3xl mb-4 text-bold mt-8">
+                                <span className="hidden md:flex">
+                                    Habits for {userWithHabits.username}
+                                </span>
+                                {userWithHabits.username === "tsheikh2001" ? (
+                                    <div className="text-bold text-blue-500">
+                                        Tablighi Edition
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                            </div>
+                            <div className="grid grid-cols-1 gap-y-6 ">
+                                {userWithHabits.habits.map((habit) => (
+                                    <Card
+                                        key={habit.id}
+                                        className={`text-white ${getColor(
+                                            habit.worship
+                                        )} transform transition-transform duration-300 hover:scale-105 cursor-pointer ring-4 ring-black`}
+                                        onClick={() =>
+                                            handleCardClick(habit.id)
+                                        }
+                                    >
+                                        <CardHeader>
+                                            <CardTitle className="text-xl text-center">
+                                                {habit.worship}
+                                            </CardTitle>
+                                        </CardHeader>
+                                        <CardContent>
+                                            <div>
+                                                <span className="hidden lg:inline">
+                                                    Daily{" "}
+                                                </span>
+                                                Quantity: {habit.dailyQuantity}{" "}
+                                                {habit.unit}
+                                                {habit.dailyQuantity > 1
+                                                    ? "s"
+                                                    : ""}
+                                            </div>
+                                            {(habit.worship === "Quran" ||
+                                                habit.worship ===
+                                                    "Salawat") && (
                                                 <div>
-                                                    <span className="hidden lg:inline">Daily </span>Quantity: {habit.dailyQuantity} {habit.unit}
-                                                    {habit.dailyQuantity > 1 ? "s" : ""}
+                                                    <span className="hidden lg:inline">
+                                                        Daily{" "}
+                                                    </span>
+                                                    Reward: {habit.reward}
                                                 </div>
-                                                {(habit.worship === "Quran" || habit.worship === "Salawat") && (
-                                                    <div><span className="hidden lg:inline">Daily </span>Reward: {habit.reward}</div>
-                                                )}
-                                            </CardContent>
-                                        </Card>
-                                    ))}
-                                </div>
+                                            )}
+                                        </CardContent>
+                                    </Card>
+                                ))}
+                            </div>
                         </div>
                     ) : (
                         <>
-                            <div>Loading...</div>
-                            <Link
-                                href="/auth/register"
-                                className={buttonVariants({
-                                    size: "sm",
-                                    className: "bg-green-600 mt-5 sm:flex items-center gap-1",
-                                })}
-                            >
-                                Register First!
-                            </Link>
+                            <div className="h-screen">
+                                <div>Loading...</div>
+                                <Link
+                                    href="/auth/register"
+                                    className={buttonVariants({
+                                        size: "lg",
+                                        className:
+                                            "bg-green-600 mt-5 sm:flex items-center gap-1 px-8",
+                                    })}
+                                >
+                                    Yet to Register?
+                                </Link>
+                            </div>
                         </>
                     )}
                     <div className="text-slate-500 mt-12 w-[400px]">
                         <p>
-                            These Rewards are calculated according to narrations recorded in{" "}
+                            These Rewards are calculated according to narrations
+                            recorded in{" "}
                             <span className="font-semibold">
                                 Tirmidhi and The Musnad of Imam Ahmad
                             </span>
-                            . Estimated rewards is a fun way of estimating your progress, but it is not a serious count. Allah(SWT) alone can accept, reject, or multiply your deeds as He wills, so make sure you have the right intentions and ask Allah(SWT) for the best of His reward.
+                            . Estimated rewards is a fun way of estimating your
+                            progress, but it is not a serious count. Allah(SWT)
+                            alone can accept, reject, or multiply your deeds as
+                            He wills, so make sure you have the right intentions
+                            and ask Allah(SWT) for the best of His reward.
                         </p>
                     </div>
                 </div>
