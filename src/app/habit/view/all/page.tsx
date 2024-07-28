d5939c80c8f69987bdf2e3da35e092c5c1af185b
hasan-huda <hasanhuda037@gmail.com>
2024-07-28 18:22:29 -0400

    styling and fixed for view all

--- src/app/habit/view/all/page.tsx
+++ src/app/habit/view/all/page.tsx
@@ -99,7 +99,7 @@ const AllHabits = () => {
                         <Plus className="ml-1.5 h-5 w-5" />
                     </Link>
                     {userWithHabits ? (
-                        <div className="w-full">
+                        <div className="w-full px-6 sm:px-2">
                                 <div className="text-3xl mb-4 text-bold mt-8">
                                     <span className="hidden md:flex">Habits for {userWithHabits.username}</span>
                                     {userWithHabits.username  === "tsheikh2001"? <div className="text-bold text-blue-500">Tablighi Edition</div> :""}
