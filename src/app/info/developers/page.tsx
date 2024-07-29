58ce730cae4b3a93db2fe3eb5b784903bcd687de
omar-t-ahmed <omartahmed@gmail.com>
2024-07-28 22:00:00 -0400

    add protfolio

--- src/app/info/developers/page.tsx
+++ src/app/info/developers/page.tsx
@@ -9,7 +9,7 @@ const Developers = () => {
       <MaxWidthWrapper className="py-4">
         <div className="flex flex-col items-center">
           <div className="font-bold text-4xl my-8">Developers</div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-24 mt-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-[128px] mt-16">
             {developers.map((developer) => (
               <DeveloperCard key={developer.name} {...developer} />
             ))}
